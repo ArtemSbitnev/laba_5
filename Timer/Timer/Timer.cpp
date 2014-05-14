8bc116892a2342682729cbdc1305916ab5ec8596
Gesser <artxxv@gmail.com>
2014-05-14 14:05:13 +0200

    first version
    very good programm

---
+++ Timer/Timer/Timer.cpp
@@ -0,0 +1,47 @@
+
+#include "stdafx.h"
+#include <string>
+using namespace std;
+
+/*int main ()
+{
+  string str ("This is an example phrase.");
+  string::iterator it;
+
+  // erase used in the same order as described above:
+  str.erase (10,8);
+  cout << str << endl;        // "This is an phrase."
+
+  it=str.begin()+9;
+  str.erase (it);
+  cout << str << endl;        // "This is a phrase."
+
+  str.erase (str.begin()+5, str.end()-7);
+  cout << str << endl;        // "This phrase."
+  system("pause");
+  return 0;
+}
+*/
+// Timer.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int main()
+{
+	int N;
+	cin>>N;
+
+	while (N!=0)
+	{cout<<N<<endl;
+	N--;
+	for (int i=0; i<115000000;i++)
+	
+	{
+	}
+	}
+	cout<<"\a!!!"<<endl;
+	system("pause");
+	return 0;
+}
